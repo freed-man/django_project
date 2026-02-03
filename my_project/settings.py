5a7692b0301d71f6cf9997feccf2aabeb289ee41
freed-man <rolandk@proton.me>
2026-02-03 20:16:15 +0000

    update

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$4p=l7pv%_s9&osh+==zffoso)fhf4(!0nd+gdp4b-^ajq@j98'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
